1e10d3c83eb465a502522e56f2001de6e1b5aae7
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-07-02 10:29:09 -0700

    inital commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'catpix'
+gem 'httparty'
+gem "tco"
+gem "rmagick"
---
+++ pic.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+require "tco"
+require "rmagick"
+require 'base64'
+res = HTTParty.get('http://media.playdota.com/hero/105/icon.jpg')
+img = Base64.encode64 res.parsed_response
+img = Magick::Image::read_inline(img).first
+
+img.each_pixel do |pixel, col, row|
+  c = [pixel.red, pixel.green, pixel.blue].map { |v| 256 * (v / 65535.0) }
+  pixel.opacity == 65535 ? print("  ") : print("  ".bg c)
+  puts if col >= img.columns - 1
+end
+puts "TINY SMASH"
